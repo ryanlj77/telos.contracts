6093525053b94ef6dc8d835f5ef805aa2fbf367a
Ed Silva <eed.silva@hotmail.com>
2018-11-26 14:26:27 -0800

    update uath inline action

--- update.auth/include/update.auth/update.auth.hpp
+++ update.auth/include/update.auth/update.auth.hpp
@@ -14,7 +14,7 @@ class [[eosio::contract("update.auth")]] uauth : public contract {
      void hello(name executer);
      
      [[eosio::action]]
-     void setperm();
+     void setperm(std::string accperm);
 
      struct permission_level_weight {
        permission_level permission;
--- update.auth/src/update.auth.cpp
+++ update.auth/src/update.auth.cpp
@@ -9,22 +9,22 @@ void uauth::hello(name executer) {
     print("\nhello executed");
 }
 
-void uauth::setperm() {
+void uauth::setperm(std::string accperm, std::string newperm ) {
     require_auth(_self);
     //TODO: send inline updateauth action to create a new permission for testaccounta (the owner of this contract.)
     action(
-        permission_level{get_self(), "active"_n}, 
+        permission_level{get_self(), name(accperm)}, 
         "eosio"_n, 
         "updateauth"_n, 
         std::make_tuple(
             get_self(),
-            "newperm"_n,
-            "active"_n,
+            name(newperm),
+            name(accperm),
             authority { 
                 1, 
                 std::vector<key_weight> {},
                 std::vector<permission_level_weight> {
-                    permission_level_weight{ permission_level{ get_self(), "newperm"_n}, 1 }
+                    permission_level_weight{ permission_level{ get_self(), "active"_n}, 1 }
                 },
                 std::vector<wait_weight> {}
             }
